e2c9d68b43a66c8d3f1536bbc55db2c030a7b969
josecasimiro4 <ze_ze2002@hotmail.com>
2011-11-14 13:18:53 +0000

    tive uns problemas com o workspace antigo, criei um novo neste

---
+++ workspace/ProductDispenser/src/Utils/OutputManager.java
@@ -0,0 +1,43 @@
+package Utils;
+
+
+public class OutputManager {
+
+	private static int _image=0x0;
+	
+	public static void setBits(int mask, int val, int pos){
+		
+		int oldImg = _image;
+		_image &= ~mask;
+		_image |= val<<pos;
+		
+		if(oldImg!=_image)
+			NotUsbPort.out(_image);
+	}
+	
+	public static void setMask(int mask)
+	{
+		int oldImg = _image;
+		_image |= mask;
+		
+		if(oldImg!=_image)
+			NotUsbPort.out(_image);
+	}
+	
+	public static void clearMask(int mask)
+	{
+		int oldImg = _image;
+		_image &= ~mask;
+		
+		if(oldImg!=_image)
+			NotUsbPort.out(_image);
+	}
+	
+	public static void toggleMask(int mask){		
+		_image ^= mask;
+		
+		NotUsbPort.out(_image);
+	}
+	
+	
+}
